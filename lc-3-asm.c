cdac4926fdbec594712df606d70dfd23698834b6
Michael Mösch <mmoesch@student.ethz.ch>
2023-03-09 16:10:33 +0100

    Creating main files.

---
+++ lc-3-asm.c
@@ -0,0 +1,18 @@
+//
+// Created by Michael on 09.03.23.
+//
+
+#include "lc-3-asm.h"
+#include <stdio.h>
+
+void printHelp(){
+    printf("Usage: blabla");
+}
+
+int main(int argc, char* argv[]){
+    if(argc==0){
+        printHelp();
+        return 0;
+    }
+    return 0;
+}
---
+++ lc-3-asm.h
@@ -0,0 +1,8 @@
+//
+// Created by Michael on 09.03.23.
+//
+
+#ifndef LC_3_ASM_LC_3_ASM_H
+#define LC_3_ASM_LC_3_ASM_H
+
+#endif //LC_3_ASM_LC_3_ASM_H
